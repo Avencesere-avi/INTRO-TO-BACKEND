e81c4f2762ab54b89a386ed7d07b6da2ba627b60
Ravi Kumar <raviavi2002@gmail.com>
2023-04-01 23:42:19 +0530

    Cohersion

---
+++ cohersion.js
@@ -0,0 +1,51 @@
+// primitive datatypes is js ---->    those datatypes which cannot be breakdown further, 
+
+// number , string , undefined , symbol , null , booleans , bigint           <------- PRIMITIVE (TYpes which are atomic (not dependent ) in nature / predefined / inbulit1)
+
+// ARRAY , OBJECT  <-------- NON - PRIMITIVE (which are formed with the help of primitive )
+
+// primtive data types                                    non - primitive data type
+
+/*
+
+
+ 1. lives in stack memory area                              1. lives in heap memory area , reference type datatypes
+ 2. they are atomic in nature means (dependent)
+ 3. 
+
+
+*/
+
+// console.log(2-undefined) // undefinded --> NaN --> 2-NaN(Invalid number ) -------> NaN 
+
+// console.log(2-null) // null ----> 0 > 2-0 > 2 ;
+
+// console.log ( 2 - true ) // 2- 1 // one 
+
+// console.log(2 - false ) // 2 -0 .. 0 ;
+
+
+// let x = "44";
+// console.log(typeof x );
+
+//  let xx = parseInt(x);
+//  console.log(xx);
+//  console.log(typeof 3 )
+
+//  console.log(1 + "0")
+
+// let a = "10"
+// let b = +a;
+
+// console.log(typeof b);
+
+
+
+
+// console.log(1 - " 0xa")
+// console.log(typeof c)
+
+
+// let x = {};
+
+// console.log(1 + x);
