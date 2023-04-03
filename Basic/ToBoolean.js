a620ebd76b671be816d35baeaa9eb8f2f50f2396
Ravi Kumar <raviavi2002@gmail.com>
2023-04-03 22:28:52 +0530

    added

--- Basic/ToBoolean.js
+++ Basic/ToBoolean.js
@@ -24,3 +24,22 @@
 // 
 // console.log(number("0xa"));
 
+// 
+// let x = NaN;
+// 
+// console.log(x == NaN);
+// 
+// console.log(isNaN(x));
+// 
+// console.log(isNan("Ravi"));
+// 
+// let obj
+
+
+let x = 10; 
+
+console.log(typeof x);
+
+console.log(x.toString());
+
+console.log(10-true);
