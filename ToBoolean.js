92a9e22cc04d1512acf8219ce8ea879a62a1a0e4
Ravi Kumar <raviavi2002@gmail.com>
2023-04-03 16:23:49 +0530

    ToBoolean

---
+++ ToBoolean.js
@@ -0,0 +1,26 @@
+
+// let x = 10;
+
+// console.log(!x)
+
+// let y = NaN;
+
+// console.log(!y);
+
+// if( 44 == "41")
+// {
+//     console.log("Tulu");
+// }
+
+// else false;
+
+// let x = {};
+// 
+// console.log(typeof x.toString());
+// 
+// console.log(1<2<3);
+// 
+// console.log(Number("abcd"))
+// 
+// console.log(number("0xa"));
+
