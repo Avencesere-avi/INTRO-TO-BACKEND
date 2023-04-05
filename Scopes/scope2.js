72187bfe0ce2ad40d8b7744e34809c10dd8a4a82
Ravi Kumar <raviavi2002@gmail.com>
2023-04-05 10:30:28 +0530

    Added code

---
+++ Scopes/scope2.js
@@ -0,0 +1,116 @@
+
+
+"use strict";
+// {
+
+//   function name() {
+//     return "123";
+//   }
+
+// }
+
+// // let x = name();
+// // console.log(name);
+
+// {
+
+//     var xx = "Ravi";
+
+// }
+// console.log(xx);
+
+
+// var x = "Ravi";
+// {
+
+//    {
+
+//              { 
+//                 f();
+                   
+
+//                 // let x = "Ravi Kumar "
+//                     function f ()
+//                     {
+//                         console.log(x);
+//                     }
+
+                    
+
+//              }
+
+//    }
+
+
+// }
+
+var x ;
+
+function x()
+{
+    return "Helloo"
+}
+
+console.log(x);
+
+
+
+
+
+
+
+
+// var y = "Ravi"
+
+//  {
+//     function f ()
+//     {
+
+//        console.log(x);
+//        var x = "Ravi Kumar";
+//        console.log(x);
+//     }
+
+
+//     // console.log(x);
+// }
+
+
+// f();
+// console.log(f());
+
+// {
+
+//     let x = 2;
+
+// }
+
+// console.log(x);
+
+// function varTest()
+// {
+ 
+//     var a =1;
+
+//     {
+
+        
+//         console.log(a);
+
+//     }
+
+//    console.log(a + "Rabi");
+
+// }
+
+// varTest();
+
+// {
+
+//   var a = "Ravi";
+//   var a = "ravi";
+
+//   console.log(a);
+
+
+// }
